ac10c5dfae05af52f1126e12355baef053337e5a
Andrew Tian <144362414+andr3wtn@users.noreply.github.com>
2025-07-13 12:05:11 +0800

    Styled About Us page

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,23 +2,24 @@ import Image from "next/image";
 
 export default function AboutUs() {
     return (
-        <section className="min-h-screen px-10 py-20">
+        <section className="min-h-screen px-10 py-10">
             <h2 className="text-4xl mb-6 text-center text-[3.875rem]">About Us</h2>
 
-            <div className="flex flex-col text-center justify-center md:flex-row">
-                <div className="w-1/2">
+            <p className="text-center text-3xl my-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <div className="flex flex-col gap-5 text-center justify-center md:flex-row items-center">
+                <div className="sm:w-4/5 md:w-1/2 flex flex-col items-center justify-center">
                     <Image src="/images/Placeholder-person.svg" alt="Person 1" width={500} height={500} />
                     <h2 className="text-[#594E32] text-4xl">Person 1</h2>
                     <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                 </div>
 
-                <div className="w-1/2">
+                <div className="sm:w-4/5 md:w-1/2 flex flex-col items-center justify-center">
                     <Image src="/images/Placeholder-person.svg" alt="Person 1" width={500} height={500} />
                     <h2 className="text-[#594E32] text-4xl">Person 2</h2>
                     <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                 </div>
 
-                <div className="w-1/2">
+                <div className="sm:w-4/5 md:w-1/2 flex flex-col items-center justify-center">
                     <Image src="/images/Placeholder-person.svg" alt="Person 1" width={500} height={500} />
                     <h2 className="text-[#594E32] text-4xl">Person 3</h2>
                     <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
